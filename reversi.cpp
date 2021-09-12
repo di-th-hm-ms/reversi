d83726bf73678da717fe588d9e538893f3a8a7f5
longtKumazo <longt.humanity@gmail.com>
2021-09-13 01:06:41 +0900

    added decorate_board()

--- reversi.cpp
+++ reversi.cpp
@@ -9,10 +9,10 @@ int BOARD[BOARD_WIDTH][BOARD_HEIGHT] = {};
 
 void make_board() {
     for (int i = 0; i < BOARD_WIDTH; ++i) {
-       BOARD[0][i] = -1;
-       BOARD[BOARD_HEIGHT - 1][i] = -1;
-       BOARD[i][0] = -1;
-       BOARD[i][BOARD_WIDTH - 1] = -1;
+       BOARD[0][i] = 0; 
+       BOARD[BOARD_HEIGHT - 1][i] = 0;
+       BOARD[i][0] = 0; 
+       BOARD[i][BOARD_WIDTH - 1] = 0;
     }
     BOARD[BOARD_HEIGHT/2 - 1][BOARD_WIDTH/2 - 1] = 1;
     BOARD[BOARD_HEIGHT/2][BOARD_WIDTH/2] = 1;
@@ -20,12 +20,37 @@ void make_board() {
     BOARD[BOARD_HEIGHT/2 - 1][BOARD_WIDTH/2] = 2;
 
 }
-int main() {
-    make_board();
+void decorate_board() {
     for (int i = 0; i < BOARD_HEIGHT; ++i) {
-        for (int j = 0; j < BOARD_WIDTH; ++j) cout << BOARD[i][j];
+        for (int j = 0; j < BOARD_WIDTH; ++j) {
+            switch (BOARD[i][j]) {
+                case 0: 
+                    cout << ".";
+					break;
+                case 1:
+                    cout << "○";
+                    break;
+                case 2:
+                    cout << "●";
+                    break;
+                case -1:
+                    //
+                    break;
+                default:
+                    break;
+                
+            }
+        }
         cout << endl;
     }
+}
+int main() {
+    make_board();
+    decorate_board();
+//    for (int i = 0; i < BOARD_HEIGHT; ++i) {
+//        for (int j = 0; j < BOARD_WIDTH; ++j) cout << BOARD[i][j];
+//        cout << endl;
+//    }
 
     return 0;
 }
