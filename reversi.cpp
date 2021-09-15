e084a48df2d09ecfe35cfb06c5c71077e5085e82
longtKumazo <longt.humanity@gmail.com>
2021-09-16 01:10:04 +0900

    modified is_enable_to_set(). not tested yet.

--- reversi.cpp
+++ reversi.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 static const int BOARD_WIDTH = 4;
 static const int BOARD_HEIGHT = 4;
+int my_piece;
 
 int BOARD[BOARD_WIDTH][BOARD_HEIGHT] = {};
+
 
 void make_board() {
     for (int i = 0; i < BOARD_WIDTH; ++i) {
@@ -16,8 +18,8 @@ void make_board() {
     }
     BOARD[BOARD_HEIGHT/2 - 1][BOARD_WIDTH/2 - 1] = 1;
     BOARD[BOARD_HEIGHT/2][BOARD_WIDTH/2] = 1;
-    BOARD[BOARD_HEIGHT/2][BOARD_WIDTH/2 - 1] = 2;
-    BOARD[BOARD_HEIGHT/2 - 1][BOARD_WIDTH/2] = 2;
+    BOARD[BOARD_HEIGHT/2][BOARD_WIDTH/2 - 1] = -1;
+    BOARD[BOARD_HEIGHT/2 - 1][BOARD_WIDTH/2] = -1;
 
 }
 void decorate_board() {
@@ -27,15 +29,12 @@ void decorate_board() {
                 case 0: 
                     cout << ".";
 					break;
-                case 1:
+                case -1:
                     cout << "○";
                     break;
-                case 2:
+                case 1:
                     cout << "●";
-                    break;
-                case -1:
-                    //
-                    break;
+                    break; 
                 default:
                     break;
                 
@@ -46,7 +45,30 @@ void decorate_board() {
 }
 // C -> int C++ -> bool
 bool is_enable_to_set(int y, int x) {
-    if (BOARD[y][x] == 0) 
+    if (BOARD[y][x] == 0) {
+        // 縦横判定 1マスずつ合計9マス確認
+        int check_x, check_y;
+        for (int i = -1; i <= 1; ++i) { // row
+            for (int j = -1; j <= 1; ++j) { // column 
+                check_x = x + j;
+                check_y = y + i;
+                if (check_x < 0 || check_x > BOARD_WIDTH - 1) continue;
+                if (check_y < 0 || check_y > BOARD_HEIGHT - 1) continue;
+                while (BOARD[check_y][check_x] == -my_piece) {
+                    // opponent's piece
+                    if (check_x < 0 || check_x > BOARD_WIDTH - 1) break;
+                    if (check_y < 0 || check_y > BOARD_HEIGHT - 1) break;
+                    
+                    if (BOARD[check_y][check_x] == my_piece) return true;
+
+                    check_y += i;
+                    check_x += j;
+                    
+                }
+            }
+        }
+    }
+    return false;
 }
 
 int main() {
@@ -54,6 +76,8 @@ int main() {
     int x, y;
     make_board();
     decorate_board();
+    my_piece = -1; // default white
+
 //    for (int i = 0; i < BOARD_HEIGHT; ++i) {
 //        for (int j = 0; j < BOARD_WIDTH; ++j) cout << BOARD[i][j];
 //        cout << endl;
